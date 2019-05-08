cc45e36effbbab62596be38f4b07e44b0c28be4e
HightMike <baobab-borisov@mail.ru>
2019-05-08 19:24:10 +0300

    31. БД. JDBC. Вывод ResultSet

--- src/main/java/spring/aop/main/mainDB.java
+++ src/main/java/spring/aop/main/mainDB.java
@@ -10,25 +10,16 @@ public class mainDB {
 
 
     public static void main(String[] args) {
-        try {
-
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
         try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); Statement statement = connection.createStatement()) {
 
-            statement.execute("INSERT into animal(anim_name, anim_col) VALUES ('name', 'desc')");
-            int res = statement.executeUpdate("UPDATE animal SET anim_name = 'tiger' where id=1; ");
             ResultSet res2 = statement.executeQuery("SELECT * from animal");
-            statement.addBatch("INSERT into animal(anim_name, anim_col) VALUES ('volf', 'desc')");
-            statement.addBatch("INSERT into animal(anim_name, anim_col) VALUES ('pantera', 'desc')");
-            statement.addBatch("INSERT into animal(anim_name, anim_col) VALUES ('leo', 'desc')");
-            statement.executeBatch();
-            statement.clearBatch();
+
+            while (res2.next()) {
+                String id = res2.getString(2);
+                System.out.println(id);
+
+            }
 
 //            System.out.println(res2);
 
