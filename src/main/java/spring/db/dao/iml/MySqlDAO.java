7a61e622ae72e9034c86130e98e0d9a4bc685b09
HightMike <baobab-borisov@mail.ru>
2019-05-19 00:11:46 +0300

    39-41. Spring. Внедрение транзакции через аспекты

--- src/main/java/spring/db/dao/iml/MySqlDAO.java
+++ src/main/java/spring/db/dao/iml/MySqlDAO.java
@@ -10,9 +10,7 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import spring.db.dao.interfaces.MP3Dao;
 import spring.db.dao.objects.MP3;
 
@@ -40,8 +38,9 @@ public class MySqlDAO implements MP3Dao {
 
 
     @Override
-    @Transactional (propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    //@Transactional (propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
     public int insert(MP3 mp3) {
+        System.out.println(TransactionSynchronizationManager.isActualTransactionActive());
 
         MapSqlParameterSource params = new MapSqlParameterSource();
         params.addValue("author", mp3.getName());
